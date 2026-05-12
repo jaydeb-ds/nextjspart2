63f33f87d4356e59ba9741ada6fd7230d22cb180
jaydeb-ds <das294761@gmail.com>
2026-05-12 19:42:57 +0530

    push

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -49,3 +49,31 @@ export async function POST(request:NextRequest) {
         console.log("register Router",error)
     }
 }
+
+export async function DELETE(request:NextRequest) 
+{
+    try {
+        await connectDB()
+        const {email} = await request.json()
+        let existuser = await User.findOne({email})
+        if(!existuser)
+        {
+            return NextResponse.json(
+                {message:`${email} this email of user not exit!`},
+                {status:400}
+            )
+        }
+        await User.deleteOne({email})
+        return NextResponse.json(
+            {message:"user deleted successfully"},
+            {status:200}
+        )
+    }
+    catch(error)
+    {
+        return NextResponse.json(
+            {message : `internal server error --> ${error}`},
+            {status:400}
+        )
+    }
+}
