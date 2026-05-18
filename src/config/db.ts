a66d52933e2c066f8ec561d25ee31ccef0ba2852
jaydeb-ds <das294761@gmail.com>
2026-05-18 21:22:06 +0530

    Re-read

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,17 +2,22 @@ import { connect, connection } from "mongoose"
 
 let mongo_db_url = process.env.MONGODB_URL
 
+// check mongoDB URL
 if(!mongo_db_url)
 {
     throw new Error("Mongodb URL is not found.") 
 }
 
+// create Cashed
 let cached = global.mongoose
 
+// Check cached 
 if(!cached)
 {
    cached = global.mongoose = {conn:null, promise:null}
 }
+
+// DB connection if conection is not found in cached
 
 const connectDB =  async () =>{
     if(cached.conn)
@@ -26,18 +31,15 @@ const connectDB =  async () =>{
     }
 
     try{
-
         cached.conn = await cached.promise
         console.log("DB connect From promised");
-
     } 
     catch(error)
     {
         throw error
     }
     return cached.conn
-
-    
+   
 }
 
 export default connectDB
